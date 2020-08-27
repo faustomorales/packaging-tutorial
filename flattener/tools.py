f9753da44df21a056dee5847ccfd980698073b26
Fausto Morales <faustomorales@gmail.com>
2020-08-26 23:28:27 -0500

    Fix linting, formatting, and add typing.

--- flattener/tools.py
+++ flattener/tools.py
@@ -1,2 +1,9 @@
-def flatten(list_of_lists):
+"""Tools that Python should have had in the first place."""
+import typing
+
+
+def flatten(list_of_lists: typing.List[typing.List[typing.Any]]) -> typing.List[typing.Any]:
+    """Flatten a list of lists."""
+    if not list_of_lists:
+        return []
     return [item for sublist in list_of_lists for item in sublist]
